55ebca1565fd6363eb32b1415e1a79f4db29802f
庄清惠 <zhuangqhc@163.com>
2016-09-08 22:59:55 +0800

    basic photo download and wallpaper setting

--- services/unsplash.js
+++ services/unsplash.js
@@ -1,9 +1,14 @@
 const rp = require('request-promise')
+const request = require('request')
+const fs = require('fs')
+const path = require('path')
+const wpUtil = require('wallpaper')
 
 class unsplash {
   constructor (APPID) {
     this.APPID = APPID
     this.BASEURL = 'https://api.unsplash.com'
+    this.photo = {}
   }
 
   _resolveUrl (url) {
@@ -17,13 +22,52 @@ class unsplash {
       json: true
     }
 
-    return rp(options)
+    rp(options)
       .then(res => {
-        return Promise.resolve(res)
+        this.photo.id = res.id
+
+        this.photo.info = {
+          created_at: res.created_at,
+          downloads: res.downloads,
+          likes: res.likes
+        }
+
+        this.photo.urls = res.urls
+        this.path = null
+
+        return this._downloadPhoto(this.photo.id, this.photo.urls.regular)
       })
       .catch(err => {
         console.log(err)
       })
+  }
+
+  _downloadPhoto (id, url) {
+    const regx = /&fm=(.*?)&/g
+    let fm = regx.exec(url)
+    if (fm) {
+      fm = fm[1]
+      this.photo.path = path.join(__dirname, '..', 'resources', 'wallpapers', `${id}.${fm}`)
+
+      return new Promise((resolve, reject) => {
+        try {
+          request(url).pipe(fs.createWriteStream(this.photo.path))
+            .on('finish', () => {
+              this.setWallpaper()
+            })
+
+          resolve()
+        } catch (e) {
+          reject(e)
+        }
+      })
+    } else {
+      return Promise.reject('format error')
+    }
+  }
+
+  setWallpaper () {
+    wpUtil.set(this.photo.path)
   }
 }
 
